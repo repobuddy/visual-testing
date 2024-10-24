e23903b979155ef397ddbdd08903e046487a9672
unional <homawong@gmail.com>
2024-10-24 16:44:10 -0700

    6

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,5 @@
 import type { Locator } from '@vitest/browser/context'
 
-export const imageSnapshotSymbol = Symbol('imageSnapshot')
-
-declare module '@vitest/browser/context' {
-	interface BrowserPage {
-		imageSnapshot(this: BrowserPage, options?: ImageSnapshotOptions): Promise<ImageSnapshot>
-	}
-}
-
 export type ImageSnapshotOptions = {
 	element?: Element | Locator
-}
-
-export type ImageSnapshot = {
-	type: typeof imageSnapshotSymbol
-	rootDir: string
-	testfilename: string
-	snapshotFilename: string
-	baselinePath: string
-	resultPath: string
-	diffPath: string
-	base64: string
-	image: ImageData
 }
