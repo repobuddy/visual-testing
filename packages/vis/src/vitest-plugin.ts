9a76df50a17aafebb076a9a4093b611712d259c8
unional <homawong@gmail.com>
2024-11-23 18:10:09 -0800

    refactor: move files

--- packages/vis/src/vitest-plugin.ts
+++ packages/vis/src/vitest-plugin.ts
@@ -1,13 +1,13 @@
 import type { Plugin } from 'vitest/config'
 import './augment.js'
+import { copyFile } from './server/commands/copy_file.js'
+import { existDir } from './server/commands/exist_dir.js'
+import { existFile } from './server/commands/exist_file.js'
+import { getSnapshotPlatform } from './server/commands/get_snapshot_platform.js'
+import { isCI } from './server/commands/is_ci.js'
+import { rmDir } from './server/commands/rm_dir.js'
 import { serverVisConext } from './server/vis_context.js'
 import type { VisOptions } from './shared/types.js'
-import { copyFile } from './vitest-plugin/commands/copy_file.js'
-import { existDir } from './vitest-plugin/commands/exist_dir.js'
-import { existFile } from './vitest-plugin/commands/exist_file.js'
-import { getSnapshotPlatform } from './vitest-plugin/commands/get_snapshot_platform.js'
-import { isCI } from './vitest-plugin/commands/is_ci.js'
-import { rmDir } from './vitest-plugin/commands/rm_dir.js'
 
 export function storybookVis(options?: VisOptions) {
 	serverVisConext.customizeSnapshotSubpath = options?.customizeSnapshotSubpath
