f55f5c65d3b7973b8369eb15a84a9c9a8731de2b
unional <unional@users.noreply.github.com>
2025-06-01 06:45:14 +0000

    deploy: d0b341579f80c86397823efbde7911f2bb47b9a8

---
+++ assets/per_story.stories-BLO0UJ5z.js
@@ -0,0 +1,7 @@
+import{j as h}from"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";const S={title:"tags/per story",render:()=>h.jsx("div",{"data-testid":"subject",children:"unit test"})},s={tags:["snapshot"]},t={tags:["!snapshot"]},a={tags:["!snapshot","snapshot"]};var r,e,o;s.parameters={...s.parameters,docs:{...(r=s.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  tags: ['snapshot']
+}`,...(o=(e=s.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};var n,p,c;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  tags: ['!snapshot']
+}`,...(c=(p=t.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var i,d,m;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  tags: ['!snapshot', 'snapshot']
+}`,...(m=(d=a.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};const l=["TakeSnapshot","SkipSnapshot","LastWin"];export{a as LastWin,t as SkipSnapshot,s as TakeSnapshot,l as __namedExportsOrder,S as default};
