f39bb414072e160b6fdaa7cf269c74b10e1f1569
unional <homawong@gmail.com>
2025-10-05 13:35:08 -0700

    test: reset images as viewport changed

--- testcases/react-vite-ts/vite.config.ts
+++ testcases/react-vite-ts/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
 		browserTestPreset({ includeGeneralTests: true }),
 	],
 	test: {
+		name: 'trv:pw',
 		browser: {
 			enabled: true,
 			headless: true,
--- testcases/themes/vite.config.ts
+++ testcases/themes/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
 		browserTestPreset({ includeGeneralTests: true }),
 	],
 	test: {
+		name: 'tt:pw',
 		browser: {
 			enabled: true,
 			headless: true,
