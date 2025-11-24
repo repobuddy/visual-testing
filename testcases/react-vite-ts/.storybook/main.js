75d6776b2b01bca0cb5c1358752222ede3b3d940
unional <homawong@gmail.com>
2025-11-24 00:58:47 -0800

    chore: update storybook and vitest configurations

--- testcases/react-vite-ts/.storybook/main.js
+++ testcases/react-vite-ts/.storybook/main.js
@@ -1,20 +1,19 @@
-import { dirname, join } from 'node:path'
+import { join } from 'node:path'
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value) {
-	return dirname(require.resolve(join(value, 'package.json')))
+	return import.meta.resolve(join(value, 'package.json'))
 }
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-
 	addons: [getAbsolutePath('@storybook/addon-vitest'), getAbsolutePath('storybook-addon-vis')],
 	framework: {
-		name: getAbsolutePath('@storybook/react-vite'),
+		name: '@storybook/react-vite',
 		options: {},
 	},
 }
--- testcases/workspace/.storybook/main.ts
+++ testcases/workspace/.storybook/main.ts
@@ -1,15 +1,12 @@
 // This file has been automatically migrated to valid ESM format by Storybook.
-import { createRequire } from "node:module";
-import { dirname, join } from 'node:path'
-
-const require = createRequire(import.meta.url);
+import { join } from 'node:path'
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value) {
-	return dirname(require.resolve(join(value, 'package.json')))
+	return import.meta.resolve(join(value, 'package.json'))
 }
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
@@ -18,11 +15,11 @@ const config = {
 
 	addons: [
 		getAbsolutePath('@storybook/addon-vitest'),
-		getAbsolutePath('storybook-addon-vis'),
 		getAbsolutePath('@storybook/addon-themes'),
+		getAbsolutePath('storybook-addon-vis'),
 	],
 	framework: {
-		name: getAbsolutePath('@storybook/react-vite'),
+		name: '@storybook/react-vite',
 		options: {},
 	},
 }
