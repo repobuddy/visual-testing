94f7e59773e91538566091308f25a216ad7731d1
Homa Wong <homawong@gmail.com>
2025-11-24 00:59:52 -0800

    fix: unify getAbsolutePath function implementation across storybook main files

--- testcases/workspace/.storybook/main.ts
+++ testcases/workspace/.storybook/main.ts
@@ -1,12 +1,12 @@
-// This file has been automatically migrated to valid ESM format by Storybook.
-import { dirname, join } from 'node:path'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value) {
-	return dirname(import.meta.resolve(join(value, 'package.json')))
+function getAbsolutePath(value: string): any {
+	return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
 }
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
