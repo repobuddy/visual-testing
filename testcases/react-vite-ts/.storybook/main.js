d8e492eaa85a963b13425cf6b0afdbf0ac1ca7ea
unional <homawong@gmail.com>
2025-11-24 00:59:52 -0800

    fix: update getAbsolutePath function in storybook main files to return directory name

--- testcases/react-vite-ts/.storybook/main.js
+++ testcases/react-vite-ts/.storybook/main.js
@@ -1,11 +1,11 @@
-import { join } from 'node:path'
+import { dirname, join } from 'node:path'
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value) {
-	return import.meta.resolve(join(value, 'package.json'))
+	return dirname(import.meta.resolve(join(value, 'package.json')))
 }
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
