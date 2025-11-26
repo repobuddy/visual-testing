2370532676741deb153c698d22d168c3a72b4ace
unional <homawong@gmail.com>
2025-11-25 22:36:14 -0800

    test: separate csf next and csf mix test case

---
+++ testcases/sb-csf-mix/.storybook/main.ts
@@ -0,0 +1,25 @@
+import { defineMain } from '@storybook/react-vite/node'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+	return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
+}
+
+export default defineMain({
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: [
+		getAbsolutePath('@storybook/addon-docs'),
+		getAbsolutePath('@storybook/addon-a11y'),
+		getAbsolutePath('@storybook/addon-vitest'),
+		getAbsolutePath('@storybook-community/storybook-dark-mode'),
+	],
+	framework: {
+		name: getAbsolutePath('@storybook/react-vite'),
+		options: {},
+	},
+})
---
+++ testcases/sb-csf-mix/README.md
@@ -0,0 +1,9 @@
+# Storybook CSF Mix Testcase
+
+This testcase is used to test using both Storybook CSF 3 and CSF Next in the same project.
+
+## Running the Testcase
+
+```bash
+pnpm t
+```
