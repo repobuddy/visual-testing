d8634333eecc28cdaa445cd9b105ea9232f46de7
unional <homawong@gmail.com>
2025-11-28 10:05:28 -0800

    Enable composite builds and configure storybook-addon-vis

--- testcases/sb-csf-mix/.storybook/vitest.setup.tsx
+++ testcases/sb-csf-mix/.storybook/vitest.setup.tsx
@@ -1,6 +1,6 @@
 import * as a11yAddonAnnotations from '@storybook/addon-a11y/preview'
 import { setProjectAnnotations } from '@storybook/react-vite'
-import { vis, visAnnotations } from 'storybook-addon-vis/vitest-setup'
+import { visAnnotations } from 'storybook-addon-vis/vitest-setup'
 import { beforeAll } from 'vitest'
 import * as projectAnnotations from './preview.tsx'
 
@@ -8,5 +8,3 @@ import * as projectAnnotations from './preview.tsx'
 const annotations = setProjectAnnotations([a11yAddonAnnotations, visAnnotations, projectAnnotations as any])
 
 beforeAll(annotations.beforeAll)
-
-vis.setup({ auto: true })
