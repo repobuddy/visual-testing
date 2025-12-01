de9bf0a6100eace35f33101d3351e938e4b8da44
unional <homawong@gmail.com>
2025-12-01 01:21:35 -0800

    feat: integrate autoSnapshotMatcher with Storybook and simplify setup

--- testcases/sb-csf-3/src/stories/Button.stories.ts
+++ testcases/sb-csf-3/src/stories/Button.stories.ts
@@ -27,6 +27,7 @@ type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
+	tags: ['!snapshot'],
 	args: {
 		primary: true,
 		label: 'Button',
