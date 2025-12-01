bf7fcecddbcef502fe71eb2c76217df7c8d037fa
unional <homawong@gmail.com>
2025-12-01 01:21:35 -0800

    feat: integrate autoSnapshotMatcher with Storybook and simplify setup

--- packages/storybook-addon-vis/src/index.ts
+++ packages/storybook-addon-vis/src/index.ts
@@ -2,7 +2,11 @@
 import './client/storybook/expect_extend.ts'
 import './shared/global_matcher_augment.ts'
 
-import { page } from './client/vitest_proxy.ts'
+import { definePreviewAddon } from 'storybook/internal/csf'
+import { expect } from 'storybook/test'
+import type { SetupVisOptions } from 'vitest-plugin-vis'
+import { autoSnapshotMatcher, setAutoSnapshotOptions } from 'vitest-plugin-vis/client-api'
+import { commands, page } from './client/vitest_proxy.ts'
 import { visAnnotations } from './preview/vis_annotation.ts'
 
 export * from 'vitest-plugin-vis'
@@ -12,9 +16,29 @@ export * from './client/storybook/tags.ts'
 
 export { page }
 
-export default (options: { auto: boolean } = { auto: false }) => {
-	return {
+export default (options: SetupVisOptions = { auto: false }) => {
+	let matcher: ReturnType<typeof autoSnapshotMatcher>
+	let matcherFn: () => Promise<void>
+	return definePreviewAddon({
 		tags: options.auto ? ['snapshot'] : [],
+		async beforeAll() {
+			matcher = autoSnapshotMatcher(commands, expect)
+			if (typeof options?.auto === 'function') {
+				matcherFn = matcher.createMatcher({
+					auto: options.auto,
+				})
+			} else if (typeof options?.auto === 'object') {
+				matcherFn = matcher.createMatcher(options.auto)
+			} else matcherFn = matcher.createMatcher({ async auto() {} })
+
+			await matcher.setup()
+			await setAutoSnapshotOptions(true)
+		},
+		afterEach() {
+			if (matcherFn) {
+				return matcherFn()
+			}
+		},
 		...visAnnotations,
-	}
+	})
 }
