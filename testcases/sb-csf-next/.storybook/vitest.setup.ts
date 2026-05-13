7d075d9628d7b6b1c46a10e080d97cdc7e97122a
unional <homawong@gmail.com>
2026-05-13 00:16:42 -0700

    refactor: simplify vitest setup in sb-csf-next test case

--- testcases/sb-csf-next/.storybook/vitest.setup.ts
+++ testcases/sb-csf-next/.storybook/vitest.setup.ts
@@ -1,13 +1,3 @@
-import * as a11yAddonAnnotations from '@storybook/addon-a11y/preview'
-import { setProjectAnnotations } from '@storybook/react-vite'
-import { vis, visAnnotations } from 'storybook-addon-vis/vitest-setup'
-import { beforeAll } from 'vitest'
-import * as projectAnnotations from './preview.tsx'
+import { vis } from 'storybook-addon-vis/vitest-setup'
 
-// This is an important step to apply the right configuration when testing your stories.
-// More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-const annotations = setProjectAnnotations([a11yAddonAnnotations, visAnnotations, projectAnnotations as any])
-
-beforeAll(annotations.beforeAll)
-
-vis.setup({ auto: true })
+vis.setup()
