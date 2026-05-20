6bf263763905ebd293a7e205c03ea3a13b03b5bd
unional <homawong@gmail.com>
2026-05-19 23:06:54 -0700

    fix: inline type augmentations in index.ts for tsdown 0.22 dts compat

--- packages/vitest-plugin-vis/src/index.ts
+++ packages/vitest-plugin-vis/src/index.ts
@@ -1,6 +1,17 @@
 // `vitest-plugin-vis` provides code that can be used in test files.
-import './client/expect/augment.ts'
-import './client/page/augment.ts'
+import type { ImageSnapshotMatcher } from './client/expect/to_match_image_snapshot.types.ts'
+import type { HasImageSnapshotAction } from './client/page/has_image_snapshot.ts'
+import type { ToMatchImageSnapshotAction } from './client/page/to_match_image_snapshot.ts'
+
+declare global {
+	namespace jest {
+		interface Matchers<R, T> extends ImageSnapshotMatcher {}
+	}
+}
+
+declare module '@vitest/browser/context' {
+	interface BrowserPage extends HasImageSnapshotAction, ToMatchImageSnapshotAction {}
+}
 
 export * from './auto_snapshots/auto_snapshot_options.ts'
 export type * from './shared/types.ts'
